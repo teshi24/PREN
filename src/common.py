7668241984f53f3e45c68b10794b60d67bd064aa
teshi24 <nadja.stadi@gmail.com>
2024-04-04 10:01:14 +0200

    extracted file writing

--- src/common.py
+++ src/common.py
@@ -7,17 +7,24 @@ import os
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging_level)
 
+
+def write_to_file(positions_json, i):
+    file_name = f"positions_config_{i}.json"
+    file_path = os.path.join('testdata', file_name)
+    with open(file_path, 'w') as file:
+        json.dump(positions_json, file)
+
+
 i = 0
 for PATH in PATHS:
     logging.info(f"analyzed video: {PATH}")
     i = i+1
-    positions = cd.analyze_cube_positions_from_video(PATH)
 
+    positions = cd.analyze_cube_positions_from_video(PATH)
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    file_name = f"positions_config_{i}.json"
-    file_path = os.path.join('testdata', file_name)
-    with open(file_path, 'w') as file:
-        json.dump({'time': time, 'config': positions}, file)
+    positions_json = {'time': time, 'config': positions}
+
+    write_to_file(positions_json, i)
 
     # break
     # red = Kontur(ImageProcessing(frame), 'red')
