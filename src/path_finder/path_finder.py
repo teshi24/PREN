855f54d6a701e07dee8396aa0ed7152b2e28892e
teshi24 <nadja.stadi@gmail.com>
2024-05-03 11:24:07 +0200

    path_finder does now find the fastes switch from one position to the other

--- src/path_finder/path_finder.py
+++ src/path_finder/path_finder.py
@@ -1,8 +1,6 @@
 from typing import Dict, Type
-from collections import deque
 import heapq
 from src.frame_analyzer.position_analyzer import PositionIdentifier
-from src.path_finder.search import Node, UndirectedGraph, GraphProblem, Graph
 
 # end_positions_percentages: Dict[int, End_Result] = {
 #     1: starting_point.copy(),
@@ -36,226 +34,95 @@ def print_graph(graph):
     pass
 
 
-def find_best_path(positions: Dict[int, Type[PositionIdentifier | None]]):
-    print(positions)
-
-    ## todo: add also the second layer
-    ## todo: add backwards mecanism
-    position_graph = Graph(dict(
-        redA=dict(redB=25, redC=50, redD=75, blueC=0, blueD=25, blueA=50, blueB=75, yellowD=0, yellowA=25, yellowB=50, yellowC=75),
-        redB=dict(redC=25, redD=50, redA=75, blueD=0, blueA=25, blueB=50, blueC=75, yellowA=0, yellowB=25, yellowC=50, yellowD=75),
-        redC=dict(redD=25, redA=50, redB=75, blueA=0, blueB=25, blueC=50, blueD=75, yellowB=0, yellowC=25, yellowD=50, yellowA=75),
-        redD=dict(redA=25, redB=50, redC=75, blueB=0, blueC=25, blueD=50, blueA=75, yellowC=0, yellowD=25, yellowA=50, yellowB=75),
-
-        blueA=dict(blueB=25, blueC=50, blueD=75, redC=0, redD=25, redA=50, redB=75, yellowD=0, yellowA=25, yellowB=50, yellowC=75),
-        blueB=dict(blueC=25, blueD=50, blueA=75, redD=0, redA=25, redB=50, redC=75, yellowA=0, yellowB=25, yellowC=50, yellowD=75),
-        blueC=dict(blueD=25, blueA=50, blueB=75, redA=0, redB=25, redC=50, redD=75, yellowB=0, yellowC=25, yellowD=50, yellowA=75),
-        blueD=dict(blueA=25, blueB=50, blueC=75, redB=0, redC=25, redD=50, redA=75, yellowC=0, yellowD=25, yellowA=50, yellowB=75),
-
-        yellowA=dict(yellowB=25, yellowC=50, yellowD=75, redD=0, redA=25, redB=50, redC=75, blueB=0, blueC=25, blueD=50, blueA=75),
-        yellowB=dict(yellowC=25, yellowD=50, yellowA=75, redA=0, redB=25, redC=50, redD=75, blueC=0, blueD=25, blueA=50, blueB=75),
-        yellowC=dict(yellowD=25, yellowA=50, yellowB=75, redB=0, redC=25, redD=50, redA=75, blueD=0, blueA=25, blueB=50, blueC=75),
-        yellowD=dict(yellowA=25, yellowB=50, yellowC=75, redC=0, redD=25, redA=50, redB=75, blueA=0, blueB=25, blueC=50, blueD=75)
-    ))
-
-    ## found redA - redD - blueA - yellowC
-    ## redA - redD = 75 - blueA - 50 - yellowC = 75
-    ## redA - yellowC = 75
-    ## ['redC', 'redD', 'redA', 'blueD', 'blueA', 'blueB', 'blueC', 'yellowA', 'yellowB', 'yellowC', 'yellowD']
-
-    # # Simplified graph dictionary initialization
-    # position_graph2 = UndirectedGraph(dict(
-    #     redA=dict(redB=25, redC=50, redD=75, blueC=0, blueD=25, blueA=50, blueB=75, yellowD=0, yellowA=25, yellowB=50, yellowC=75),
-    #     redB=dict(redC=25, redD=50, blueD=0, blueA=25, blueB=50, blueC=75, yellowA=0, yellowB=25, yellowC=50, yellowD=75),
-    #     redC=dict(redD=25, blueA=0, blueB=25, blueC=50, blueD=75, yellowB=0, yellowC=25, yellowD=50, yellowA=75),
-    #     redD=dict(blueB=0, blueC=25, blueD=50, blueA=75, yellowC=0, yellowD=25, yellowA=50, yellowB=75),
-    #     blueA=dict(blueB=25, blueC=50, blueD=75, yellowD=0, yellowA=25, yellowB=50, yellowC=75),
-    #     blueB=dict(blueC=25, blueD=50, yellowA=0, yellowB=25, yellowC=50, yellowD=75),
-    #     blueC=dict(blueD=25, yellowB=0, yellowC=25, yellowD=50, yellowA=75),
-    #     blueD=dict(yellowC=0, yellowD=25, yellowA=50, yellowB=75),
-    #     yellowA=dict(yellowB=25, yellowC=50, yellowD=75),
-    #     yellowB=dict(yellowC=25, yellowD=50),
-    #     yellowC=dict(yellowD=25),
-    # ))
-
-    start = ['redA']
-    possible_goals = ['yellowC']
-    problem = GraphProblem(start, possible_goals, position_graph)
-    goal_node = uniform_cost_search(problem)
-    evaluate(goal_node)
-    # todo: fix
-    pass
+position_graph = {
+    'redC': [('blueA', 0), ('yellowD', 0),
+             ('redD', 25), ('redA', 50), ('redB', 75)],
+    'redD': [('blueB', 0), ('yellowA', 0),
+             ('redA', 25), ('redB', 50), ('redC', 75)],
+    'redA': [('blueC', 0), ('yellowB', 0),
+             ('redB', 25), ('redC', 50), ('redD', 75)],
+    'redB': [('blueD', 0), ('yellowC', 0),
+             ('redA', 25), ('redB', 50), ('redC', 75)],
+    'blueA': [('redC', 0), ('yellowD', 0),
+              ('blueB', 25), ('blueC', 50), ('blueD', 75)],
+    'blueB': [('redD', 0), ('yellowA', 0),
+              ('blueC', 25), ('blueD', 50), ('blueA', 75)],
+    'blueC': [('redA', 0), ('yellowB', 0),
+              ('blueD', 25), ('blueA', 50), ('blueB', 75)],
+    'blueD': [('redB', 0), ('yellowC', 0),
+              ('blueA', 25), ('blueB', 50), ('blueC', 75)],
+    'yellowD': [('redC', 0), ('blueA', 0),
+                ('yellowA', 25), ('yellowB', 50), ('yellowC', 75)],
+    'yellowA': [('redD', 0), ('blueB', 0),
+                ('yellowB', 25), ('yellowC', 50), ('yellowD', 75)],
+    'yellowB': [('redA', 0), ('blueC', 0),
+                ('yellowC', 25), ('yellowD', 50), ('yellowA', 75)],
+    'yellowC': [('redB', 0), ('blueD', 0),
+                ('yellowD', 25), ('yellowA', 50), ('yellowB', 75)],
+}
 
 
-def uniform_cost_search(problem):
-    node = Node(problem.initial)
-    visited = set()
-    if problem.goal_test(node.state):
-        return node
-    frontier = []
-    heapq.heappush(frontier, (node.path_cost, node))
-    while frontier:
-        print('------------')
-        for item in frontier:
-            print(item[0])
-            print(item[1].state)
-        print('------------')
-        node = heapq.heappop(frontier)[1]
-        if problem.goal_test(node.state):
-            return node
-        visited.add(node.state)
-        for action in problem.get_actions_from(node.state):
-            child = node.create_child_node(problem, action)
-            if child.state not in visited and child.state not in frontier:
-                heapq.heappush(frontier, (child.path_cost, child))
-                visited.add(child.state)
-            elif any(n[1].state == child.state and n[1].path_cost > child.path_cost for n in frontier):
-                # Find the frontier node with the same state as child but with a higher path_cost
-                for i, (cost, frontier_node) in enumerate(frontier):
-                    if frontier_node.state == child.state and frontier_node.path_cost > child.path_cost:
-                        # Replace the frontier node with child
-                        frontier[i] = (child.path_cost, child)
-                        break
-    return False
+def find_best_path(positions):
+    start_node = 'redA'
+    goal_node = 'yellowB'
+    ## should be direct and 0 - it is, just total costs are fucked up
+    start_node = 'redA'
+    goal_node = 'yellowC'
+    # ## should be over yellowB 0 and yellowC = 25 - it is
+    start_node = 'blueA'
+    goal_node = 'blueD'
+    # ## should be direct and 75 - it was
+    start_node = 'yellowB'
+    goal_node = 'redD'
+    # ## should be over redA 0 and redD = 75
+    start_node = 'yellowD'
+    goal_node = 'redB'
+    # should be 75
 
-
-# code from aiso
-
-# def uniform_cost_search(problem):
-#     node = Node(problem.initial)
-#     visited = set()
-#     current_path_costs = set()
-#     if problem.goal_test(node.state):
-#         return node
-#     frontier = []
-#     print(f'node {node}')
-#     print(f'node.state {node.state}')
-#     print(f'node.path_cost {node.path_cost}')
-#     heapq.heappush(frontier, (node.path_cost, [node]))
-#     current_path_costs.add(node.path_cost)
-#     while True:
-#         print('entered while')
-#         if not frontier:
-#             return False
-#         print('------------')
-#         for item in frontier:
-#             print(item[0])
-#             print(item[1])
-#         print('------------')
-#         # todo: this needs to be able to handle a list
-#         nodes = heapq.heappop(frontier)[1]
-#         if problem.goal_test(nodes):
-#             return nodes
-#         for node in nodes:
-#             visited.add(node.state)
-#
-#         node = nodes[0] ## todo: check if sufficient; should actually be, bc. the states with the same costs are pointing to the same directions
-#         print(node.state)
-#         print(problem.get_actions_from(node.state))
-#         for action in problem.get_actions_from(node.state):
-#             print(action)
-#             child = node.create_child_node(problem, action)
-#             if child.state not in visited and child.state not in frontier:
-#                 if child.path_cost in current_path_costs:
-#                     print('duplicate detected')
-#                     print(f'current_path_costs {current_path_costs}')
-#                     print(f'child.path_cost {child.path_cost}')
-#                     ## update the list
-#                     for i, (cost, frontier_nodes) in enumerate(frontier):
-#                         if cost == child.path_cost:
-#                             frontier_nodes.append(child)
-#                             frontier[i] = (child.path_cost, frontier_nodes)
-#                             break
-#                     visited.add(child.state)
-#                     continue  # todo: handle duplicates better - bc, we need to be able to stick 2 items at the same time
-#                 heapq.heappush(frontier, (child.path_cost, [child]))
-#                 current_path_costs.add(child.path_cost)
-#                 visited.add(child.state)
-#
-#             # todo: needs to be able to handle a list of states
-#             elif any(n[1].state == child.state and n[1].path_cost > child.path_cost for n in frontier):
-#                 for i, (cost, frontier_node) in enumerate(frontier):
-#                     if frontier_node.state == child.state and frontier_node.path_cost > child.path_cost:
-#                         frontier[i] = (child.path_cost, child)
-#                         break
-
-
-# def uniform_cost_search(problem):
-#     node = Node(problem.initial)
-#     visited = set()
-#     if problem.goal_test(node.state):
-#         return node
-#
-#     frontier = []
-#     heapq.heappush(frontier, (node.path_cost, node))
-#     # Use a dictionary to track the best path cost to a node found so far.
-#     frontier_states = {node.state: node.path_cost}
-#
-#     while frontier:
-#         print('------------')
-#         for item in frontier:
-#             print(item[0], item[1].state)
-#         print('------------')
-#
-#         # Get the node with the lowest cost
-#         node = heapq.heappop(frontier)[1]
-#         visited.add(node.state)
-#
-#         # Check for goal after popping it off the frontier
-#         if problem.goal_test(node.state):
-#             return node
-#
-#         print(node.state)
-#         print(problem.get_actions_from(node.state))
-#
-#         for action in problem.get_actions_from(node.state):
-#             print(action)
-#             child = node.create_child_node(problem, action)
-#
-#             # Check if a cheaper path to the state has been found
-#             if child.state not in visited or (
-#                     child.state in frontier_states and frontier_states[child.state] > child.path_cost):
-#                 # Update the state's cost in frontier_states
-#                 frontier_states[child.state] = child.path_cost
-#                 heapq.heappush(frontier, (child.path_cost, child))
-#                 print(f'Pushing to frontier: {child.state} with path cost {child.path_cost}')
-#
-#                 # Remove the old node if it's still in the frontier
-#                 if child.state in visited:
-#                     visited.remove(child.state)
-#
-#     return False
-
-def test():
-    romania_graph = UndirectedGraph(dict(
-        Arad=dict(Zerind=75, Sibiu=140, Timisoara=118),
-        Bucharest=dict(Urziceni=85, Pitesti=101, Giurgiu=90, Fagaras=211),
-        Craiova=dict(Drobeta=120, Rimnicu=146, Pitesti=138),
-        Drobeta=dict(Mehadia=75),
-        Eforie=dict(Hirsova=86),
-        Fagaras=dict(Sibiu=99),
-        Hirsova=dict(Urziceni=98),
-        Iasi=dict(Vaslui=92, Neamt=87),
-        Lugoj=dict(Timisoara=111, Mehadia=70),
-        Oradea=dict(Zerind=71, Sibiu=151),
-        Pitesti=dict(Rimnicu=97),
-        Rimnicu=dict(Sibiu=80),
-        Urziceni=dict(Vaslui=142)))
-
-    start = 'Sibiu'
-    goal = 'Bucharest'
-    problem = GraphProblem(start, goal, romania_graph)
-    goal_node = uniform_cost_search(problem)
-    evaluate(goal_node)
-
-
-def evaluate(node):
-    if node:
-        print("The search algorithm reached " + node.state + " with a cost of " + str(node.path_cost) + ".")
-        print("The actions that led to the solutions are the following: ")
-        print(node.get_solution())
-        print(f"The algorithm visited {len(node.get_solution())} cities")
-        print(f'depth = {node.depth}')
-        print(f'path cost = {node.path_cost}')
+    total_cost, path_with_costs = bidirectional_uniform_cost_search_with_costs(position_graph, start_node, goal_node)
+    if path_with_costs:
+        print("Total Cost from", start_node, "to", goal_node, ":", total_cost)
+        print("Path with Step Costs:")
+        for node, step_cost in path_with_costs:
+            print("Node:", node, "- Step Cost:", step_cost)
     else:
-        print('no solution found')
+        print("No path found from", start_node, "to", goal_node)
+
+
+def bidirectional_uniform_cost_search_with_costs(graph, start, goal):
+    # Priority queues for forward and backward searches
+    forward_frontier = [(0, start)]  # (cost, node) for forward search
+    forward_cost_so_far = {start: 0}
+
+    # Dictionaries to store the parent node of each node in the final path from both directions
+    forward_came_from = {}
+    forward_current_node = start
+
+    while forward_frontier and forward_current_node is not goal:
+        # Forward search
+        forward_current_cost, forward_current_node = heapq.heappop(forward_frontier)
+
+        for next_node, cost in graph[forward_current_node]:
+            new_cost = forward_current_cost + cost
+            if next_node not in forward_cost_so_far or new_cost < forward_cost_so_far[next_node]:
+                forward_cost_so_far[next_node] = new_cost
+                heapq.heappush(forward_frontier, (new_cost, next_node))
+                forward_came_from[next_node] = (forward_current_node, cost)
+
+    # Reconstruct the path from start to goal
+    forward_path = []
+    print(f'forward_cost_so_far {forward_cost_so_far}')
+    print(f'forward_cost_so_far.values() {forward_cost_so_far.values()}')
+    print(f'forward_came_from {forward_came_from}')
+    total_cost = sum(forward_cost_so_far.values())
+    # Construct forward path
+    current_node = goal
+    while current_node in forward_came_from:
+        parent_node, step_cost = forward_came_from[current_node]
+        forward_path.insert(0, (current_node, step_cost))
+        current_node = parent_node
+        ## todo: add up costs here
+    forward_path.insert(0, (start, 0))  # Add start node with cost 0
+
+    print(forward_path)
+    return total_cost, forward_path
